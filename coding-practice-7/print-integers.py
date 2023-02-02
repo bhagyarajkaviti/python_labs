18536915cd668bdfd9b02c6d7d94eb74b3a04806
bhagyarajkaviti <bhagyarajkaviti@gmail.com>
2023-02-02 12:25:27 +0530

    27jan files

---
+++ coding-practice-7/print-integers.py
@@ -0,0 +1,8 @@
+#Write a program to print integers from 1 to the given integer (N).
+integer = int(input())
+
+count = 1
+
+while count <= integer:
+    print(count)
+    count = count + 1
---
+++ coding-practice-7/read-n-inputs.py
@@ -0,0 +1,9 @@
+#Given an integer N, write a program which reads N inputs and prints them.
+integer = int(input())
+
+count = 0
+
+while count < integer:
+    a = int(input())
+    count = count + 1
+    print(a)
