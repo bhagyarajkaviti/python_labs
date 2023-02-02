f2f386a3f282f9d1d33b169ff50552373192792c
bhagyarajkaviti <109148786+bhagyarajkaviti@users.noreply.github.com>
2023-02-02 09:11:13 +0530

    Revert "20jan files"

--- coding-practice-3A/equal-numbers.py
+++
@@ -1,8 +0,0 @@
-#Write a program to check if the given two numbers are equal.
-number1 = int(input())
-number2 = int(input())
-
-if number1 == number2:
-    print("Equal")
-else:
-    print("Not Equal")
--- coding-practice-3A/relation-between-numbers.py
+++
@@ -1,8 +0,0 @@
-#Write a program to print the relation between two numbers, X and Y.
-X = int(input())
-Y = int(input())
-
-if X < Y:
-    print("X < Y")
-else:
-    print("X >= Y")
